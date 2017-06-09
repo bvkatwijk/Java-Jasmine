89a2455924e1af622fe4f375e6a42f2c0c142f5f
bvkatwijk <bvkatwijk@gmail.com>
2017-06-09 17:36:31 +0200

    Add single describe test

---
+++ src/test/java/org/bvkatwijk/java/jasmine/api/test/SingleDescribeTest.java
@@ -0,0 +1,19 @@
+package org.bvkatwijk.java.jasmine.api.test;
+
+import org.bvkatwijk.java.jasmine.api.test.JasmineTest;
+
+public class SingleDescribeTest extends JasmineTest {{
+
+	describe("Suite 1 / 1", () -> {
+
+		it("Test 1 / 2", () -> {
+
+		});
+
+		it("Test 2 / 2", () -> {
+
+		});
+
+	});
+
+}}
