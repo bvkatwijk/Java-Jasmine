8e4df2562f2dd7a038d4c3732405a315f2ca205a
bvkatwijk <bvkatwijk@gmail.com>
2017-06-09 17:36:23 +0200

    Add example test

---
+++ src/test/java/org/bvkatwijk/java/jasmine/api/test/ExampleTest.java
@@ -0,0 +1,13 @@
+package org.bvkatwijk.java.jasmine.api.test;
+
+public class ExampleTest extends JasmineTest {{
+
+	describe("A suite", () -> {
+
+		it("should run its testcase", () -> {
+
+		});
+
+	});
+
+}}
