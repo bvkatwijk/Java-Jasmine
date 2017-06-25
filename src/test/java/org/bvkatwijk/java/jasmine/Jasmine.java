eadb14fb68f2fcce2a9046c8817eeee45ba2aff7
bvkatwijk <bvkatwijk@gmail.com>
2017-06-25 23:26:02 +0200

    Jasmine enum now takes runnable

--- src/test/java/org/bvkatwijk/java/jasmine/Jasmine.java
+++ src/test/java/org/bvkatwijk/java/jasmine/Jasmine.java
@@ -1,6 +1,6 @@
 package org.bvkatwijk.java.jasmine;
 
-import java.util.function.Supplier;
+import java.util.function.Function;
 
 import org.bvkatwijk.java.jasmine.api.test.JasmineInternalTest;
 import org.bvkatwijk.java.jasmine.compiled.JasmineGroup;
@@ -10,52 +10,56 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Jasmine {
 
-	DESCRIBE_IT(() -> new JasmineInternalTest(){{
+	DESCRIBE_IT((run) -> new JasmineInternalTest(){{
 		describe("describe", () -> {
 			it("it", () -> {
-				//
+				run.run();
 			});
 		});
 	}}),
 
-	DESCRIBE_FIT(() -> new JasmineInternalTest(){{
+	DESCRIBE_FIT((run) -> new JasmineInternalTest(){{
 		describe("describe", () -> {
 			fit("fit", () -> {
-				//
+				run.run();
 			});
 		});
 	}}),
 
-	DESCRIBE_XIT(() -> new JasmineInternalTest(){{
+	DESCRIBE_XIT((run) -> new JasmineInternalTest(){{
 		describe("describe", () -> {
 			xit("xit", () -> {
-				//
+				run.run();
 			});
 		});
 	}}),
 
-	FDESCRIBE_IT(() -> new JasmineInternalTest(){{
+	FDESCRIBE_IT((run) -> new JasmineInternalTest(){{
 		fdescribe("fdescribe", () -> {
 			it("it", () -> {
-				//
+				run.run();
 			});
 		});
 	}}),
 
-	XDESCRIBE_IT(() -> new JasmineInternalTest(){{
+	XDESCRIBE_IT((run) -> new JasmineInternalTest(){{
 		xdescribe("xdescribe", () -> {
 			it("it", () -> {
-				//
+				run.run();
 			});
 		});
 	}}),
 
 	;
 
-	private final Supplier<JasmineInternalTest> jasmineTestSupplier;
+	private final Function<Runnable, JasmineInternalTest> jasmineTestSupplier;
 
 	public JasmineInternalTest getJasmineTest() {
-		return jasmineTestSupplier.get();
+		return jasmineTestSupplier.apply(() -> {});
+	}
+	
+	public JasmineInternalTest getJasmineTest(Runnable runnable) {
+		return jasmineTestSupplier.apply(runnable);
 	}
 
 	public JasmineGroup getJasmineGroup() {
