60da25ce663b0ab351f94976e7d24a1f359597fa
bvkatwijk <bvkatwijk@gmail.com>
2017-06-25 17:45:57 +0200

    Modify Jasmine to have suppliers instead of shared instances

--- src/test/java/org/bvkatwijk/java/jasmine/Jasmine.java
+++ src/test/java/org/bvkatwijk/java/jasmine/Jasmine.java
@@ -1,60 +1,65 @@
 package org.bvkatwijk.java.jasmine;
 
+import java.util.function.Supplier;
+
 import org.bvkatwijk.java.jasmine.api.test.JasmineTest;
 import org.bvkatwijk.java.jasmine.compiled.JasmineGroup;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum Jasmine {
-	
-	DESCRIBE_IT(new JasmineTest(){{
+
+	DESCRIBE_IT(() -> new JasmineTest(){{
 		describe("describe", () -> {
 			it("it", () -> {
 				//
 			});
 		});
 	}}),
-	
-	DESCRIBE_FIT(new JasmineTest(){{
+
+	DESCRIBE_FIT(() -> new JasmineTest(){{
 		describe("describe", () -> {
 			fit("fit", () -> {
 				//
 			});
 		});
 	}}),
-	
-	DESCRIBE_XIT(new JasmineTest(){{
+
+	DESCRIBE_XIT(() -> new JasmineTest(){{
 		describe("describe", () -> {
 			xit("xit", () -> {
 				//
 			});
 		});
 	}}),
-	
-	FDESCRIBE_IT(new JasmineTest(){{
+
+	FDESCRIBE_IT(() -> new JasmineTest(){{
 		fdescribe("fdescribe", () -> {
 			it("it", () -> {
 				//
 			});
 		});
 	}}),
-	
-	XDESCRIBE_IT(new JasmineTest(){{
+
+	XDESCRIBE_IT(() -> new JasmineTest(){{
 		xdescribe("xdescribe", () -> {
 			it("it", () -> {
 				//
 			});
 		});
 	}}),
-	
+
 	;
-	
-	@Getter private final JasmineTest jasmineTest;
-	
+
+	private final Supplier<JasmineTest> jasmineTestSupplier;
+
+	public JasmineTest getJasmineTest() {
+		return jasmineTestSupplier.get();
+	}
+
 	public JasmineGroup getJasmineGroup() {
-		return jasmineTest.compile();
+		return jasmineTestSupplier.get().compile();
 	}
 
 }
