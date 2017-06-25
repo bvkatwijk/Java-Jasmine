18ecf457c6807662d0135317dce5a14e80f71e94
bvkatwijk <bvkatwijk@gmail.com>
2017-06-25 23:28:29 +0200

    Add method to RunValidationTest

--- src/test/java/org/bvkatwijk/java/jasmine/runner/RunValidationTest.java
+++ src/test/java/org/bvkatwijk/java/jasmine/runner/RunValidationTest.java
@@ -2,7 +2,7 @@ package org.bvkatwijk.java.jasmine.runner;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import org.bvkatwijk.java.jasmine.api.test.JasmineInternalTest;
+import org.bvkatwijk.java.jasmine.Jasmine;
 import org.bvkatwijk.java.jasmine.runner.decide.JasmineGroupDecider;
 import org.bvkatwijk.java.jasmine.utils.Mocks;
 import org.junit.Assert;
@@ -13,16 +13,16 @@ public abstract class RunValidationTest {
 	
 	@Getter private final AtomicBoolean didRun = new AtomicBoolean(false);
 
-	public void assertDidRun(JasmineInternalTest describeIt) {
-		Assert.assertTrue(runAndGetResult(describeIt));
+	public void assertDidRun(Jasmine jasmine) {
+		Assert.assertTrue(runAndGetResult(jasmine));
 	}
 
-	public void assertDidNotRun(JasmineInternalTest describeIt) {
-		Assert.assertFalse(runAndGetResult(describeIt));
+	public void assertDidNotRun(Jasmine jasmine) {
+		Assert.assertFalse(runAndGetResult(jasmine));
 	}
 
-	private boolean runAndGetResult(JasmineInternalTest describeIt) {
-		JasmineGroupDecider.of(Mocks.getRunNotifier(), describeIt.compile()).process();
+	private boolean runAndGetResult(Jasmine jasmine) {
+		JasmineGroupDecider.of(Mocks.getRunNotifier(), jasmine.getJasmineTest(() -> getDidRun().set(true)).compile()).process();
 		return didRun.get();
 	}
 
