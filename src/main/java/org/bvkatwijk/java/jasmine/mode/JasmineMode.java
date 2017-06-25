405047db1f907c2887bcfdae7421cb142b7ffca0
bvkatwijk <bvkatwijk@gmail.com>
2017-06-25 22:35:53 +0200

    Register filters on JasmineMode

--- src/main/java/org/bvkatwijk/java/jasmine/mode/JasmineMode.java
+++ src/main/java/org/bvkatwijk/java/jasmine/mode/JasmineMode.java
@@ -1,17 +1,30 @@
 package org.bvkatwijk.java.jasmine.mode;
 
+import java.util.function.Predicate;
+
+import org.bvkatwijk.java.jasmine.api.prefix.Prefix;
+import org.bvkatwijk.java.jasmine.compiled.JasmineNode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 /**
  * Various mode the JasmineRunner can run in.
  */
+@Getter
+@RequiredArgsConstructor
 public enum JasmineMode {
 	
 	/**
 	 * Normal Mode: Execute all groups except ignored ones
 	 */
-	NORMAL,
+	NORMAL(node -> !node.getPrefix().equals(Prefix.X)),
 	
 	/**
 	 * Focus mode: Execute only focused groups
 	 */
-	FOCUS;
+	FOCUS(node -> node.getPrefix().equals(Prefix.F));
+	
+	private final Predicate<JasmineNode> filter;
+
 }
