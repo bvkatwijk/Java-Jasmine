6cfac72fe3a7aba55c429239b92d57ccea2aa916
bvkatwijk <bvkatwijk@gmail.com>
2017-06-25 13:50:14 +0200

    Move example test to example package

--- src/test/java/org/bvkatwijk/java/jasmine/example/ExampleTest.java
+++ src/test/java/org/bvkatwijk/java/jasmine/example/ExampleTest.java
@@ -1,12 +1,13 @@
-package org.bvkatwijk.java.jasmine.api.test;
+package org.bvkatwijk.java.jasmine.example;
 
+import org.bvkatwijk.java.jasmine.api.test.JasmineTest;
 import org.junit.Assert;
 
 public class ExampleTest extends JasmineTest {{
 
 	describe("A suite", () -> {
 		it("should run its testcase", () -> {
-			Assert.assertEquals(true, 1 == 1);
+			Assert.assertEquals(true, true);
 		});
 	});
 
