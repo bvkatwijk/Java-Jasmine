efa39783815b89a0c66bd02c9a07e3ad7353252a
bvkatwijk <bvkatwijk@gmail.com>
2017-06-25 21:33:20 +0200

    JasmineGroup uses JasmineInternalTest

--- src/main/java/org/bvkatwijk/java/jasmine/compiled/JasmineGroup.java
+++ src/main/java/org/bvkatwijk/java/jasmine/compiled/JasmineGroup.java
@@ -7,7 +7,7 @@ import java.util.stream.Stream;
 
 import org.bvkatwijk.java.jasmine.api.describe.Describe;
 import org.bvkatwijk.java.jasmine.api.prefix.Prefix;
-import org.bvkatwijk.java.jasmine.api.test.JasmineTest;
+import org.bvkatwijk.java.jasmine.api.test.JasmineInternalTest;
 import org.bvkatwijk.java.jasmine.name.ClassNameProvider;
 
 import lombok.Builder;
@@ -24,14 +24,14 @@ public class JasmineGroup implements JasmineNode {
 	private final Collection<JasmineGroup> groups;
 	private final Collection<JasmineCase> cases;
 
-	public static JasmineGroup of(JasmineTest jasmineTest) {
+	public static JasmineGroup of(JasmineInternalTest jasmineTest) {
 		return recursiveFrom(
 				jasmineTest,
 				ClassNameProvider.getClassName(jasmineTest.getClass()),
 				Prefix.NONE);
 	}
 
-	private static JasmineGroup recursiveFrom(JasmineTest jasmineTest, String description, Prefix prefix) {
+	private static JasmineGroup recursiveFrom(JasmineInternalTest jasmineTest, String description, Prefix prefix) {
 		return JasmineGroup.builder()
 				.description(description)
 				.prefix(prefix)
@@ -40,14 +40,14 @@ public class JasmineGroup implements JasmineNode {
 				.build();
 	}
 
-	private static Collection<JasmineGroup> toJasmineGroups(JasmineTest jasmineTest, String parentDescription) {
+	private static Collection<JasmineGroup> toJasmineGroups(JasmineInternalTest jasmineTest, String parentDescription) {
 		return new HashSet<>(jasmineTest.getDescribes())
 				.stream()
 				.map(describe -> toJasmineGroup(jasmineTest, parentDescription, describe))
 				.collect(Collectors.toSet());
 	}
 
-	private static JasmineGroup toJasmineGroup(JasmineTest jasmineTest, String parentDescription, Describe describe) {
+	private static JasmineGroup toJasmineGroup(JasmineInternalTest jasmineTest, String parentDescription, Describe describe) {
 		jasmineTest.getDescribes().clear();
 		jasmineTest.getIts().clear();
 
@@ -58,7 +58,7 @@ public class JasmineGroup implements JasmineNode {
 				describe.getPrefix());
 	}
 
-	private static Collection<JasmineCase> toJasmineCases(JasmineTest jasmineTest, String description, Prefix prefix) {
+	private static Collection<JasmineCase> toJasmineCases(JasmineInternalTest jasmineTest, String description, Prefix prefix) {
 		return jasmineTest.getIts()
 				.stream()
 				.map(testCase -> JasmineCase.toJasmineCase(testCase, description))
