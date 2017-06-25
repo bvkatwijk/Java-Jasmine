c4cae21f869330797127b2ddd9e21b65decd65f2
bvkatwijk <bvkatwijk@gmail.com>
2017-06-25 17:36:32 +0200

    Add initial JasmineDescriptionTest

---
+++ src/test/java/org/bvkatwijk/java/jasmine/runner/description/JasmineDescriptionTest.java
@@ -0,0 +1,17 @@
+package org.bvkatwijk.java.jasmine.runner.description;
+
+import org.bvkatwijk.java.jasmine.Jasmine;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JasmineDescriptionTest {
+
+	@Test
+	public void description_ofAnonymousClass_hasDisplayNameJasmineTest() {
+		Assert.assertEquals("JasmineTest",
+				new JasmineDescription(Jasmine.DESCRIBE_IT.getJasmineGroup())
+				.get()
+				.getDisplayName());
+	}
+
+}
