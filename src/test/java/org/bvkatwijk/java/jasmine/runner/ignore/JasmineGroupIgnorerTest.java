3fdda2d7c4c9ead0f0041476345b27c46c2a6a98
bvkatwijk <bvkatwijk@gmail.com>
2017-06-25 16:23:06 +0200

    Add test for JasmineGroupIgnorer

---
+++ src/test/java/org/bvkatwijk/java/jasmine/runner/ignore/JasmineGroupIgnorerTest.java
@@ -0,0 +1,26 @@
+package org.bvkatwijk.java.jasmine.runner.ignore;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.bvkatwijk.java.jasmine.api.prefix.Prefix;
+import org.bvkatwijk.java.jasmine.compiled.JasmineCase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.notification.RunNotifier;
+
+/**
+ * Test cases for {@link JasmineGroupIgnorer}
+ */
+public class JasmineGroupIgnorerTest {
+	
+	private final AtomicBoolean didRun = new AtomicBoolean(false);
+
+	@Test
+	public void ignorer_shouldNotRunJasmineCase() {
+		new JasmineGroupIgnorer()
+		.ignoreIt(new RunNotifier(), "className")
+		.accept(new JasmineCase("description", Prefix.NONE, () -> { didRun.set(true); }));
+		
+		Assert.assertFalse(didRun.get());
+	}
+}
