60ae98d212ce79bed79e6a530973b6bac9ab2a54
bvkatwijk <bvkatwijk@gmail.com>
2017-06-25 23:29:15 +0200

    Tweak JasmineGroupDecider

--- src/main/java/org/bvkatwijk/java/jasmine/runner/decide/JasmineGroupDecider.java
+++ src/main/java/org/bvkatwijk/java/jasmine/runner/decide/JasmineGroupDecider.java
@@ -13,7 +13,9 @@ import org.junit.runner.notification.RunNotifier;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Getter
 @RequiredArgsConstructor
 public class JasmineGroupDecider {
@@ -22,17 +24,16 @@ public class JasmineGroupDecider {
 	private final JasmineGroup source;
 	private final JasmineGroupIgnorer ignorer;
 	private final JasmineGroupRunner runner;
-	
+
 	public static JasmineGroupDecider of(RunNotifier runNotifier, JasmineGroup jasmineGroup) {
-		return new JasmineGroupDecider(
-				runNotifier,
-				jasmineGroup,
-				new JasmineGroupIgnorer(jasmineGroup.getDescription()),
-				new JasmineGroupRunner());
+		return new JasmineGroupDecider(runNotifier, jasmineGroup,
+				new JasmineGroupIgnorer(jasmineGroup.getDescription()), new JasmineGroupRunner());
 	}
-	
+
 	public void process() {
-		runCasesAndSubGroups(runNotifier, new JasmineModeDecider(source).get()).accept(source);
+		JasmineMode jasmineMode = new JasmineModeDecider(source).get();
+		log.info("Running " + source.getDescription() + " in mode " + jasmineMode);
+		runCasesAndSubGroups(runNotifier, jasmineMode).accept(source);
 	}
 
 	private Consumer<? super JasmineGroup> runCasesAndSubGroups(RunNotifier runNotifier, JasmineMode jasmineMode) {
@@ -41,22 +42,28 @@ public class JasmineGroupDecider {
 			jasmineGroup.getGroups().forEach(processGroup(runNotifier, jasmineMode));
 		};
 	}
-	
+
 	private Consumer<? super JasmineGroup> processGroup(RunNotifier runNotifier, JasmineMode jasmineMode) {
-		return jasmineGroup -> { 
-			if(shouldRun(jasmineMode, jasmineGroup)) {
+		return jasmineGroup -> {
+			log.info("Deciding " + jasmineGroup.getDescription() + " for mode " + jasmineMode);
+			if (shouldRun(jasmineMode, jasmineGroup)) {
+				log.info("Running " + jasmineGroup.getDescription() + " for mode " + jasmineMode);
 				runCasesAndSubGroups(runNotifier, jasmineMode).accept(jasmineGroup);
 			} else {
+				log.info("Ignoring " + jasmineGroup.getDescription() + " for mode " + jasmineMode);
 				ignorer.ignoreGroup(runNotifier).accept(jasmineGroup);
 			}
 		};
 	}
-	
+
 	private Consumer<? super JasmineCase> processCase(RunNotifier runNotifier, JasmineMode jasmineMode) {
-		return jasmineCase -> { 
-			if(shouldRun(jasmineMode, jasmineCase)) {
+		return jasmineCase -> {
+			log.info("Deciding " + jasmineCase.getDescription() + " for mode " + jasmineMode);
+			if (shouldRun(jasmineMode, jasmineCase)) {
+				log.info("Running " + jasmineCase.getDescription() + " for mode " + jasmineMode);
 				runner.runIt(runNotifier, source.getDescription()).accept(jasmineCase);
 			} else {
+				log.info("Ignoring " + jasmineCase.getDescription() + " for mode " + jasmineMode);
 				ignorer.ignoreCase(runNotifier).accept(jasmineCase);
 			}
 		};
