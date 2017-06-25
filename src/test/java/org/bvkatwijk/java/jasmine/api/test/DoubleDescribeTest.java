c769bdb0478e6bd2f680a6ac3ff9635739f460b4
bvkatwijk <bvkatwijk@gmail.com>
2017-06-25 13:50:39 +0200

    Remove test bodies in DoubleDescribeTest

--- src/test/java/org/bvkatwijk/java/jasmine/api/test/DoubleDescribeTest.java
+++ src/test/java/org/bvkatwijk/java/jasmine/api/test/DoubleDescribeTest.java
@@ -1,13 +1,11 @@
 package org.bvkatwijk.java.jasmine.api.test;
 
-import org.bvkatwijk.java.jasmine.api.test.JasmineTest;
-
 public class DoubleDescribeTest extends JasmineTest {{
 
 	describe("Suite 1 / 2", () -> {
 
 		fit("Test 1 / 1", () -> {
-			throw new IllegalStateException("e");
+			//
 		});
 
 	});
@@ -15,7 +13,7 @@ public class DoubleDescribeTest extends JasmineTest {{
 	fdescribe("Suite 2 / 2", () -> {
 
 		xit("Test 1 / 1", () -> {
-
+			//
 		});
 
 	});
