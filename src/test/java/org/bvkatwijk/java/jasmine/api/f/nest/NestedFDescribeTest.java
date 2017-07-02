bb934fc50936fc5b528c5eae23464acc65a0bfcc
bvkatwijk <bvkatwijk@gmail.com>
2017-07-02 12:06:45 +0200

    Add NestedFDescribeTest

---
+++ src/test/java/org/bvkatwijk/java/jasmine/api/f/nest/NestedFDescribeTest.java
@@ -0,0 +1,81 @@
+package org.bvkatwijk.java.jasmine.api.f.nest;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.bvkatwijk.java.jasmine.api.test.JasmineInternalTest;
+import org.bvkatwijk.java.jasmine.runner.decide.JasmineGroupDecider;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.notification.RunNotifier;
+import org.mockito.Mockito;
+
+import lombok.Getter;
+
+public class NestedFDescribeTest {
+
+	@Getter private static final AtomicBoolean d_it_didRun = new AtomicBoolean(false);
+	@Getter private static final AtomicBoolean d_fd_it_didRun = new AtomicBoolean(false);
+	@Getter private static final AtomicBoolean d_fd_d_it_didRun = new AtomicBoolean(false);
+	@Getter private static final AtomicBoolean d_d_it_didRun = new AtomicBoolean(false);
+	@Getter private static final AtomicBoolean d_d_d_it_didRun = new AtomicBoolean(false);
+
+	private static final JasmineInternalTest TEST = new JasmineInternalTest(){{
+		describe("describe", () -> {
+			it("it", () -> {
+				d_it_didRun.set(true);
+			});
+			fdescribe("fdescribe", () -> {
+				it("it", () -> {
+					d_fd_it_didRun.set(true);
+				});
+				describe("describe", () -> {
+					it("it", () -> {
+						d_fd_d_it_didRun.set(true);
+					});
+				});
+			});
+			describe("fdescribe", () -> {
+				it("it", () -> {
+					d_d_it_didRun.set(true);
+				});
+				describe("describe", () -> {
+					it("it", () -> {
+						d_d_d_it_didRun.set(true);
+					});
+				});
+			});
+		});
+	}};
+	
+	@BeforeClass
+	public static void runTest() {
+		JasmineGroupDecider.of(Mockito.mock(RunNotifier.class), TEST.compile()).process();
+	}
+	
+	@Test
+	public void d_it_didRun_shouldBeFalse() {
+		Assert.assertFalse(d_it_didRun.get());
+	}
+	
+	@Test
+	public void d_fd_it_didRun_shouldBeTrue() {
+		Assert.assertTrue(d_fd_it_didRun.get());
+	}
+	
+	@Test
+	public void d_fd_d_it_didRun_shouldBeTrue() {
+		Assert.assertTrue(d_fd_d_it_didRun.get());
+	}
+	
+	@Test
+	public void d_d_it_didRun_shouldBeFalse() {
+		Assert.assertFalse(d_d_it_didRun.get());
+	}
+	
+	@Test
+	public void d_d_d_it_didRun_shouldBeFalse() {
+		Assert.assertFalse(d_d_d_it_didRun.get());
+	}
+
+}
