41707a217bdc3d5b33b1b55e9e24cbed71c0d714
bvkatwijk <bvkatwijk@gmail.com>
2017-07-02 12:21:16 +0200

    Extract method tryToRun in JasmineGroupRunner

--- src/main/java/org/bvkatwijk/java/jasmine/runner/run/JasmineGroupRunner.java
+++ src/main/java/org/bvkatwijk/java/jasmine/runner/run/JasmineGroupRunner.java
@@ -7,22 +7,28 @@ import org.junit.runner.Description;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunNotifier;
 
+import lombok.RequiredArgsConstructor;
+
 /**
  * Executor for {@link JasmineCase}
  */
+@RequiredArgsConstructor
 public class JasmineGroupRunner {
+	
+	private final String sourceDescription;
 
-	public Consumer<? super JasmineCase> runIt(RunNotifier runNotifier, String className) {
-		return it -> {
-			Description description = Description.createTestDescription(className, it.getDescription());
-			try {
-				runNotifier.fireTestStarted(description);
-				it.getRunnable().run();
-			} catch (Throwable e) {
-				runNotifier.fireTestFailure(new Failure(description, e));
-			}
-			runNotifier.fireTestFinished(description);
-		};
+	public Consumer<? super JasmineCase> runIt(RunNotifier runNotifier) {
+		return it -> tryToRun(runNotifier, it.getRunnable(), Description.createTestDescription(sourceDescription, it.getDescription()));
+	}
+
+	private void tryToRun(RunNotifier runNotifier, Runnable runnable, Description description) {
+		try {
+			runNotifier.fireTestStarted(description);
+			runnable.run();
+		} catch (Throwable e) {
+			runNotifier.fireTestFailure(new Failure(description, e));
+		}
+		runNotifier.fireTestFinished(description);
 	}
 
 }
