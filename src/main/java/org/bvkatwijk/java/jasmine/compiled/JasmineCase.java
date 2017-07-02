fc636d57ae919fe3e85159b4510533daaa5257c0
bvkatwijk <bvkatwijk@gmail.com>
2017-07-02 12:49:41 +0200

    Remove static conversion in group and case

--- src/main/java/org/bvkatwijk/java/jasmine/compiled/JasmineCase.java
+++ src/main/java/org/bvkatwijk/java/jasmine/compiled/JasmineCase.java
@@ -3,7 +3,6 @@ package org.bvkatwijk.java.jasmine.compiled;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import org.bvkatwijk.java.jasmine.api.it.It;
 import org.bvkatwijk.java.jasmine.api.prefix.Prefix;
 
 import lombok.Getter;
@@ -16,13 +15,6 @@ public class JasmineCase implements JasmineNode {
 	private final String description;
 	private final Prefix prefix;
 	private final Runnable runnable;
-
-	public static JasmineCase toJasmineCase(It it, String parentDescription) {
-		return new JasmineCase(
-				it.getDescription(),
-				it.getPrefix(),
-				it.getTestCase());
-	}
 
 	@Override
 	public Collection<JasmineNode> getChilden() {
--- src/main/java/org/bvkatwijk/java/jasmine/compiled/JasmineGroup.java
+++ src/main/java/org/bvkatwijk/java/jasmine/compiled/JasmineGroup.java
@@ -1,14 +1,10 @@
 package org.bvkatwijk.java.jasmine.compiled;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.bvkatwijk.java.jasmine.api.describe.Describe;
 import org.bvkatwijk.java.jasmine.api.prefix.Prefix;
-import org.bvkatwijk.java.jasmine.api.test.JasmineInternalTest;
-import org.bvkatwijk.java.jasmine.utils.ClassNameProvider;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -23,48 +19,6 @@ public class JasmineGroup implements JasmineNode {
 	private final Prefix prefix;
 	private final Collection<JasmineGroup> groups;
 	private final Collection<JasmineCase> cases;
-
-	public static JasmineGroup of(JasmineInternalTest jasmineTest) {
-		return recursiveFrom(
-				jasmineTest,
-				ClassNameProvider.getClassName(jasmineTest.getClass()),
-				Prefix.NONE);
-	}
-
-	private static JasmineGroup recursiveFrom(JasmineInternalTest jasmineTest, String description, Prefix prefix) {
-		return JasmineGroup.builder()
-				.description(description)
-				.prefix(prefix)
-				.cases(toJasmineCases(jasmineTest, description, prefix))
-				.groups(toJasmineGroups(jasmineTest, description))
-				.build();
-	}
-
-	private static Collection<JasmineGroup> toJasmineGroups(JasmineInternalTest jasmineTest, String parentDescription) {
-		return new HashSet<>(jasmineTest.getDescribes())
-				.stream()
-				.map(describe -> toJasmineGroup(jasmineTest, parentDescription, describe))
-				.collect(Collectors.toSet());
-	}
-
-	private static JasmineGroup toJasmineGroup(JasmineInternalTest jasmineTest, String parentDescription, Describe describe) {
-		jasmineTest.getDescribes().clear();
-		jasmineTest.getIts().clear();
-
-		describe.getTestGroup().run();
-
-		return JasmineGroup.recursiveFrom(
-				jasmineTest,
-				describe.getDescription(),
-				describe.getPrefix());
-	}
-
-	private static Collection<JasmineCase> toJasmineCases(JasmineInternalTest jasmineTest, String description, Prefix prefix) {
-		return jasmineTest.getIts()
-				.stream()
-				.map(testCase -> JasmineCase.toJasmineCase(testCase, description))
-				.collect(Collectors.toSet());
-	}
 
 	@Override
 	public Collection<JasmineNode> getChilden() {
