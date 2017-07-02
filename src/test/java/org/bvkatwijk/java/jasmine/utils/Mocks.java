a1fcdde5b5ac1d9d45884f810ef2474e80285fb4
bvkatwijk <bvkatwijk@gmail.com>
2017-07-02 12:30:07 +0200

    Add Mock Runner

--- src/test/java/org/bvkatwijk/java/jasmine/utils/Mocks.java
+++ src/test/java/org/bvkatwijk/java/jasmine/utils/Mocks.java
@@ -1,14 +1,23 @@
 package org.bvkatwijk.java.jasmine.utils;
 
+import java.util.function.Consumer;
+
+import org.bvkatwijk.java.jasmine.api.test.JasmineInternalTest;
+import org.bvkatwijk.java.jasmine.runner.decide.JasmineGroupDecider;
 import org.junit.runner.notification.RunNotifier;
 import org.mockito.Mockito;
 
 public final class Mocks {
 
+	/** Mocked test runner for {@link JasmineInternalTest} */
+	public static Consumer<JasmineInternalTest> getMockRunner() {
+		return internalTest -> JasmineGroupDecider.of(getRunNotifier(), internalTest.compile()).process();
+	}
+
 	public static RunNotifier getRunNotifier() {
 		return Mockito.mock(RunNotifier.class);
 	}
-	
+
 	private Mocks() {
 		throw new IllegalStateException(this.getClass().getSimpleName() + " not to be instantiated.");
 	}
